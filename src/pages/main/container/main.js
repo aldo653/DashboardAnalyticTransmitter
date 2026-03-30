ec5132d5fca6515b9ec4c1d632ea41849ffa26c2
Muhammad Aldo Saputra <maldosptra563@gmail.com>
2026-03-30 17:42:17 +0700

    add data-realtime 1

--- src/pages/main/container/main.js
+++ src/pages/main/container/main.js
@@ -10,6 +10,7 @@ import Image from "next/image";
 import LoadingPage from "@/providers/loading";
 import { getLastData, getData } from "../../api/function/getData";
 
+
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
